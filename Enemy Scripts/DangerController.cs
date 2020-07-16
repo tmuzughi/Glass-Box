fefd2aa94b99678bd16f80b0c4c6cbde15b16b3a
Tarik Muzughi <55818129+tmuzughi@users.noreply.github.com>
2020-07-15 22:15:06 -0500

    Add files via upload

---
+++ Enemy Scripts/DangerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player(Clone)")
+        {
+            GameObject spawn = GameObject.FindGameObjectWithTag("Respawn");
+            PlayerDeath script = spawn.GetComponent<PlayerDeath>();
+            script.timeToDie = true;
+        }
+    }
+}
---
+++ Enemy Scripts/SpikeController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour
+{
+    private int holdUp;
+    public int holdUpDuration;
+    private int wait;
+    private int holdDown;
+    public int holdDownDuration;
+    public int waitDuration;
+    private float size;
+    private bool flipSwitch;
+    public float upRate;
+    public float downRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector2(size, size);
+
+        
+        if (wait >= waitDuration)
+        {         
+            if (size < 1 && !flipSwitch)
+            {
+                size += upRate;
+                if (size >= 1)
+                    size = 1;
+                
+            }
+            if (size > 0 && flipSwitch)
+            {
+                size -= downRate;
+                if (size <= 0)
+                    size = 0;
+            }
+            if (size == 0)
+            {
+                holdDown += 1;
+                if (holdDown >= holdDownDuration)
+                {
+                    
+                        flipSwitch = false;
+
+                    holdDown = 0;
+                }
+            }
+            if (size == 1)
+            {
+                holdUp += 1;
+                if (holdUp >= holdUpDuration)
+                {
+
+                    flipSwitch = true;
+
+                    holdUp = 0;
+                }
+            }
+        }
+        else
+        {
+            wait += 1;
+        }
+    }
+}
